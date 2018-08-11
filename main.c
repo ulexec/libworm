0291f28487688ec2c3f3586af048569bd4951989
nachosu <ignacio@bitlackeys.org>
2018-08-12 00:18:23 +0300

    initial commit

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ int main(int argc, char **argv) {
 
     if (!(bin = load_elf("/home/ulexec/a.out", PROT_READ | PROT_WRITE, MAP_PRIVATE))) {
         fprintf(stderr, "load_elf failed");
+        return -1;
     }
 
     bin_iter_phdrs(iter, bin) {
         Elfx_Phdr *phdr = list_entry(iter, Elfx_Phdr, list);
         printf("0x%lx\n" , phdr->data->p_paddr);
     }
-
     bin_iter_shdrs(iter, bin) {
         Elfx_Shdr *shdr = list_entry(iter, Elfx_Shdr, list);
         printf("%s\n", get_section_name(bin, shdr));
